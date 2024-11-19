85e9255bf8826c4f990f2f28c4b61a386773f01a
Adeel Shahid <adeel.shhid@gmail.com>
2024-11-19 19:49:31 +0500

    android issues are fixed

--- src/android/PrintManager.java
+++ src/android/PrintManager.java
@@ -27,9 +27,6 @@ import android.graphics.Bitmap;
 import android.print.PrintAttributes;
 import android.print.PrintDocumentAdapter;
 import android.print.PrintJob;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.print.PrintHelper;
 import android.webkit.CookieManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -44,6 +41,10 @@ import static android.content.Context.PRINT_SERVICE;
 import static android.os.Build.VERSION.SDK_INT;
 import static android.print.PrintJobInfo.STATE_COMPLETED;
 import static de.appplant.cordova.plugin.printer.PrintContent.ContentType.UNSUPPORTED;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.print.PrintHelper;
 
 /**
  * Provides high level methods for printing.
